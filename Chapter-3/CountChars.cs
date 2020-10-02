7b2d9042c6e71830bf942af4c983c7617edb6a7b
Kristian <jettkristian@gmail.com>
2020-10-01 18:44:43 -0700

    Add project files.

---
+++ Chapter-3/CountChars.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter_3
+{
+    class CountChars
+    {
+
+        public static Dictionary<char, int> countEachChar(string input)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach(char c in input)
+            {
+                if(char.IsLetter(c))
+                {
+                    char upperCase = char.ToUpper(c);
+                    if (!(dict.ContainsKey(upperCase)))
+                    {
+                        dict[upperCase] = 0;
+                    }
+
+                    dict[upperCase]++;
+                }
+                
+
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ Chapter-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to get a character count:");
+            string userInput = Console.ReadLine();
+
+            Dictionary<char, int> countFromInput = CountChars.countEachChar(userInput);
+
+            foreach (KeyValuePair<char, int> kvp in countFromInput)
+            {
+                Console.WriteLine($"{kvp.Key} : {kvp.Value}");
+            }
+
+            countFromInput.Select(i => $"{i.Key} : {i.Value}").ToList().ForEach(Console.WriteLine);
+        
+        }
+    }
+}
